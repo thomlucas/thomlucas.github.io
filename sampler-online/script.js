6de98ac17635ea0e112c0d800b70ebc46cd678a1
Thomas LUCAS <thomas_lucas@live.fr>
2024-06-25 14:40:01 +0200

    sampler online

--- sampler-online/script.js
+++ sampler-online/script.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const containers = document.querySelectorAll('.container');
+    const lastInteractionTimes = {};  // Stocke le temps de la dernière interaction pour chaque image
 
     function playAudioAndAnimate(image, audio) {
         audio.currentTime = 0;
@@ -21,8 +22,18 @@ document.addEventListener('DOMContentLoaded', function() {
     containers.forEach(function(container) {
         const image = container.querySelector('img');
         const audioId = image.id.replace('image-', 'audio-');
+        
+        function handleInteraction(event) {
+            const now = Date.now();
+            const lastInteractionTime = lastInteractionTimes[image.id] || 0;
 
-        function handleInteraction() {
+            // Vérifier si l'interaction précédente a eu lieu il y a moins de 300 ms
+            if (now - lastInteractionTime < 300) {
+                return;
+            }
+
+            lastInteractionTimes[image.id] = now;
+
             const audio = document.getElementById(audioId);
             if (audio) {
                 playAudioAndAnimate(image, audio);
